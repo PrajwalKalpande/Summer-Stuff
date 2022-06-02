0e0ca3330bf5f1fb16259c4d2fcc82b946ed1975
Prajwal Kalpande <prajwalkalpande3@gmail.com>
2022-06-02 20:20:19 +0530

    Solving Striver only

--- Striver Sheet/Next_Permutation.cpp
+++ Striver Sheet/Next_Permutation.cpp
@@ -4,10 +4,38 @@ int main(){
 
 }
 
-vector<int> nextPermutation(vector<int> &permutation, int n)
-{
-   
-
-	
-	
-}
+class Solution {
+public:
+    void nextPermutation(vector<int>& a) {
+        int temp=-1;
+        int val;
+        int n = a.size();
+        for(int i = n-1; i>0;i--){
+            if(a[i]>a[i-1]){temp= i;break;}
+    
+        }
+        
+        if(temp==-1){
+            sort(a.begin(),a.end());
+            return;
+        }
+        temp--;
+        int x =a[temp];
+        int num=n;
+        for(int i=temp+1;i<n;i++){
+            if(a[i]<=x){
+                num=i;
+                break;
+            }
+        }
+        num--;
+        swap(a[num],a[temp]);
+       
+        sort(a.begin()+temp+1,a.end());
+        return;
+        
+       
+        
+        
+    }
+};
